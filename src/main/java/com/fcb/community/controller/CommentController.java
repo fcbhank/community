777f211c8c1b62508fd87ea14995b8c2d76c00ad
HankHu20170822 <hankhu163@foxmail.com>
2019-07-08 19:17:55 +0800

    实现回复功能-初识API

---
+++ src/main/java/com/fcb/community/controller/CommentController.java
@@ -0,0 +1,7 @@
+package com.fcb.community.controller;
+
+/**
+ * Created by hank on 7/8/19
+ */
+public class CommentController {
+}
---
+++ src/main/java/com/fcb/community/dto/CommentDTO.java
@@ -0,0 +1,11 @@
+package com.fcb.community.dto;
+
+import lombok.Data;
+
+/**
+ * Created by hank on 7/8/19
+ */
+@Data
+public class CommentDto {
+    private 
+}
